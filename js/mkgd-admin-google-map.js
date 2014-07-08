2315c5ed9dcaa5a80e52cd07807bd145620a8a68
manoj_rana91986 <manoj_rana91986@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-08 04:56:31 +0000

    Delete js
    git-svn-id: https://plugins.svn.wordpress.org/google-distance-calculator/trunk@944698 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/mkgd-admin-google-map.js
+++
@@ -1,8 +0,0 @@
-var autocomplete;
-function initialize() {
-  var default_location = document.getElementById('autocomplete');
-  autocomplete = new google.maps.places.Autocomplete(default_location, {types: ['geocode']});
-  google.maps.event.addListener(autocomplete, 'place_changed', function() {    
-  });
-}
-window.onload = initialize();
